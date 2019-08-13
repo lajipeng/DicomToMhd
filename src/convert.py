404b185c4ccc27eb234b01034bbf6978ad6135a5
lajipeng <45223567+lajipeng@users.noreply.github.com>
2019-08-13 18:49:02 +0800

    get add convert.py

--- src/convert.py
+++ src/convert.py
@@ -20,7 +20,7 @@ import sys
 from adjust_window import window_adjust
 
  
-
+#%%
 # Path Statement
 
 PathDicom = "E:/Research/Liver 20181228/#2"		# where to save dicom files 
@@ -29,7 +29,11 @@ SaveRawDicom = "E:/Research/Liver 20181228 mhd/#2"     # where to save mhd and r
 
 lstFilesDCM = []
 F1 = os.listdir(PathDicom)
+count = 0
 for i in F1:
+    count = count + 1
+    if(count<=10):
+        continue    
     F2 = os.listdir(PathDicom + '/' + i)
     F1_i = i
     s = []
@@ -48,28 +52,28 @@ for i in F1:
                                 #print(filename)
 
                                 lstFilesDCM.append(F1_j + '/' + filename)  # put it into list
-
+                                
  
-
+#%%
         # 1：The first dcm will be the reference and all dcms have the same dimensions
 
         RefDs = pydicom.read_file(lstFilesDCM[0])  # read the first dicom image
         
 
          
-
+#%%
         # 2：Get the dimensions of dicom image
 
         ConstPixelDims = (int(RefDs.Rows), int(RefDs.Columns), len(lstFilesDCM)) # ConstPixelDimsis a tuple
 
-         
+#%%      
 
         # 3：Get the Spacing along x and y and the SliceThick along z
 
         ConstPixelSpacing = (float(RefDs.PixelSpacing[0]), float(RefDs.PixelSpacing[1]), float(RefDs.SliceThickness))
 
          
-
+#%%
         # 4：Get the Origin of dicom image
 
         Origin = RefDs.ImagePositionPatient
@@ -79,11 +83,10 @@ for i in F1:
         ArrayDicom = numpy.zeros(ConstPixelDims, dtype=RefDs.pixel_array.dtype)  # array is a numpy array
 
          
-
+#%%
         # 5: Read all the dicom image in the list and put the pixel_array in the 3D-array
 
-        i = 0
-
+        lstFilesDCM.clear()
         for filenameDCM in lstFilesDCM:
                 #print(filenameDCM)
                 ds = pydicom.read_file(filenameDCM)
@@ -91,16 +94,15 @@ for i in F1:
                 pixel_array = window_adjust(ds)
                 #print(pixel_array)
                 ArrayDicom[:, :, lstFilesDCM.index(filenameDCM)] = pixel_array
-                i += 1
+        
+        
 
          
-
+#%%
         # 6：Transpose the numpy array
 
         ArrayDicom = numpy.transpose(ArrayDicom, (2, 0, 1))
-
-         
-
+#%%
         # 7: Transform the numpy array into mhd and raw through SimpleITK
 
         sitk_img = sitk.GetImageFromArray(ArrayDicom, isVector=False)
@@ -112,6 +114,3 @@ for i in F1:
         sitk.WriteImage(sitk_img, F2_j)
 
         print("%s successfully transformed!" % F1_j)
-
-        sys.exit(0)
-
